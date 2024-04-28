0da4c115dc274ef1ae1f9a47cbfed17c5fad6ee4
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2024-04-28 17:40:27 -0400

    bug fix

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ const path = require("path");
 const IsGitHubURL = require("is-github-url");
 const download = require("download");
 
-function compareVersions(compareURL, force) {
+function CompareVersions(compareURL, force) {
+  let packageJson,
+    currentVersion,
+    version,
+    minor,
+    major,
+    patch,
+    additional,
+    latestVersion,
+    latest;
   console.log("compareVersions");
   let packageData;
   try {
@@ -16,7 +25,6 @@ function compareVersions(compareURL, force) {
 
   if (!packageData) if (!force) return 0;
 
-  let packageJson, currentVersion, version, minor, major, patch, additional;
   if (!force) {
     packageJson = JSON.parse(packageData);
 
@@ -50,8 +58,9 @@ function compareVersions(compareURL, force) {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        let latestVersion = data.tag_name;
-        let latest = latestVersion.split(".");
+        console.log("Fetched latest version from GitHub");
+        latestVersion = data.tag_name;
+        latest = latestVersion.split(".");
         let latestMinor, latestMajor, latestPatch, latestAdditional;
         if (latest.length < 3) {
           console.error("Version in package.json is not in correct format");
@@ -68,7 +77,7 @@ function compareVersions(compareURL, force) {
         }
 
         if (!force) {
-          let state = compareSemantic(latest, version);
+          let state = compareSemantic(latest, version)[0];
 
           if (state === "outdated") {
             console.log("Current version is outdated");
@@ -89,73 +98,74 @@ function compareVersions(compareURL, force) {
 
   function compareSemantic(VerNew, VerOld) {
     let state;
-    let latestMajor = parseInt(VerNew[0]);
-    let latestMinor = parseInt(VerNew[1]);
-    let latestPatch = parseInt(VerNew[2]);
 
-    let major = parseInt(VerOld[0]);
-    let minor = parseInt(VerOld[1]);
-    let patch = parseInt(VerOld[2]);
+    let newVersion = [];
+    let oldVersion = [];
+    let oldAdditional = oldVersion.slice(3, oldVersion.length - 1);
+    let newAdditional = newVersion.slice(3, newVersion.length - 1);
 
-    if (latestMajor > major) {
-      console.log("Major version is outdated");
+    VerNew.forEach((elem) => {
+      newVersion.push(elem.replace(/\D/g, ""));
+    });
+    VerOld.forEach((elem) => {
+      oldVersion.push(elem.replace(/\D/g, ""));
+    });
+
+    let latestMajor = parseInt(newVersion[0]);
+    let latestMinor = parseInt(newVersion[1]);
+    let latestPatch = parseInt(newVersion[2]);
+
+    let major = parseInt(oldVersion[0]);
+    let minor = parseInt(oldVersion[1]);
+    let patch = parseInt(oldVersion[2]);
+
+    console.log(latestMajor, latestMinor, latestPatch);
+    //console.log(major, minor, patch);
+
+    if (latestMajor > major || latestMinor > minor || latestPatch > patch) {
+      console.log("GitHub version is newer than local version");
       state = "outdated";
-    } else if (latestMajor < major) {
-      console.log("Major version is up-to-date");
+    } else if (
+      latestMajor <= major ||
+      latestMinor <= minor ||
+      latestPatch <= patch
+    ) {
+      console.log("GitHub version is older than local version");
       state = "up-to-date";
-    } else {
-      if (latestMinor > minor) {
-        console.log("Minor version is outdated");
-        state = "outdated";
-      } else if (latestMinor < minor) {
-        console.log("Minor version is up-to-date");
-        state = "up-to-date";
-      } else {
-        if (latestPatch > patch) {
-          console.log("Patch version is outdated");
-          state = "outdated";
-        } else if (latestPatch < patch) {
-          console.log("Patch version is up-to-date");
-          state = "up-to-date";
-        } else {
-          console.log("All versions are up-to-date");
-          state = "up-to-date";
-        }
-      }
     }
-    console.log(`Current Version => ${version} \nLatest Version => ${latest}`);
+    let completeOldVersion = version.join(".");
+    let completeNewVersion = latest.join(".");
+    console.log(
+      `\n\nCurrent Version => ${completeOldVersion} \nLatest Version => ${completeNewVersion}\n`
+    );
 
     return [state, version, latest];
   }
 }
 
 module.exports = {
-  compareVersions: (compareURL, force) => compareVersions(compareURL, force)[0],
+  compareVersions: (compareURL, force) => CompareVersions(compareURL, force),
 
-  autoUpdater: ({
+  autoUpdater: (
     GitHubURL,
-    GitHubBranch,
-    tempLocation,
-    exitWhenComplete,
-    force,
-    doubleBackup,
-  }) => {
+    { tempLocation, exitWhenComplete, force, doubleBackup }
+  ) => {
     console.log("autoUpdater");
-    let state;
-    if (!force) state = compareVersions(GitHubURL, force)[0];
+    let metadata = CompareVersions(GitHubURL, force);
+    console.log(metadata);
+    if (!force) state = metadata[0];
 
     if (
-      IsGitHubURL(GitHubURL, { repository: true }) &&
-      GitHubBranch &&
-      IsGitHubURL(`${GitHubURL}/tree/${GitHubBranch}`, { repository: true })
+      IsGitHubURL(GitHubURL, { repository: true })
     ) {
       if (force) state = "outdated";
 
       if (state === "outdated") {
         console.log("Downloading latest version");
 
-        const url = GitHubURL;
-        const RepoName = url.split("/")[url.split("/").length - 1];
+        let url = GitHubURL.split("/");
+        const RepoName = url[url.length - 1];
+        const owner = url[url.length - 2];
 
         let temp, tempFile;
 
@@ -167,64 +177,76 @@ module.exports = {
           return 0;
         }
 
-        let zipURL = `${url}/archive/refs/heads/${GitHubBranch}.zip`;
+        url = `https://api.github.com/repos/${owner}/${RepoName}/releases/latest`;;
+        
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            zipURL = data.zipball_url;
 
-        download(zipURL, temp, { extract: true }).then(() => {
-          console.log("Downloaded Latest Extracted Version");
-          if (tempLocation) {
-            try {
-              console.log("Saving current directory to temp directory...");
-              fs.cpSync(process.cwd(), temp + "/backup", { recursive: true })
-                .then(() => {
-                  console.log("Saved current directory to temp directory");
-                })
-                .catch((e) => {
-                  console.error(
-                    "Error in saving current directory to temp directory\n"
+            download(zipURL, temp, { extract: true, filename:"tempDirectory" }).then(() => {
+                console.log("Downloaded Latest Extracted Version");
+                if (tempLocation) {
+                  try {
+                    console.log("Saving current directory to temp directory...");
+                    fs.cpSync(process.cwd(), temp + "/backup", { recursive: true })
+                      .then(() => {
+                        console.log("Saved current directory to temp directory");
+                      })
+                      .catch((e) => {
+                        console.error(
+                          "Error in saving current directory to temp directory\n"
+                        );
+                        console.log(e);
+                        return 0;
+                      });
+                  } catch (e) {
+                    console.error(
+                      "Error in saving current directory to temp directory\n"
+                    );
+                    console.log(e);
+                    return 0;
+                  }
+                }
+      
+                if (doubleBackup)
+                  console.log(
+                    "Deleting current directory and starting second backup..."
                   );
+                else console.log("Deleting current directory...");
+      
+                try {
+                  fs.rmSync(process.cwd(), { recursive: true });
+                } catch (e) {
+                  console.error("Error in deleting current directory\n");
                   console.log(e);
                   return 0;
-                });
-            } catch (e) {
-              console.error(
-                "Error in saving current directory to temp directory\n"
-              );
-              console.log(e);
-              return 0;
-            }
-          }
-
-          if (doubleBackup)
-            console.log(
-              "Deleting current directory and starting second backup..."
-            );
-          else console.log("Deleting current directory...");
-
-          try {
-            fs.rmSync(process.cwd(), { recursive: true });
-          } catch (e) {
-            console.error("Error in deleting current directory\n");
+                }
+      
+                console.log("Deleted current directory");
+      
+                console.log("Extracting latest version...");
+      
+                try {
+                  fs.cpSync(temp + `/tempDirectory`, process.cwd(), {
+                    recursive: true,
+                  });
+                } catch (e) {
+                  console.error("Error in extracting latest version\n");
+                  console.log(e);
+                  return 0;
+                }
+      
+                console.log("Extracted latest version");
+                if (exitWhenComplete) process.exit(0);
+              });
+          })
+          .catch((e) => {
+            console.error("Error in fetching latest version\n");
             console.log(e);
             return 0;
-          }
-
-          console.log("Deleted current directory");
-
-          console.log("Extracting latest version...");
-
-          try {
-            fs.cpSync(temp + `/${RepoName}-${GitHubBranch}`, process.cwd(), {
-              recursive: true,
-            });
-          } catch (e) {
-            console.error("Error in extracting latest version\n");
-            console.log(e);
-            return 0;
-          }
-
-          console.log("Extracted latest version");
-          if (exitWhenComplete) process.exit(0);
-        });
+          });
       } else if (state === "up-to-date") {
         console.log("No updates available");
         return 0;
